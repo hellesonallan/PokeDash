1688ad64af3197490f0e099d7263cebd96248ac9
Helleson Allan <113473921+hellesonallan@users.noreply.github.com>
2025-06-03 00:40:12 -0300

    chore: chave api

---
+++ config/config.js
@@ -0,0 +1,6 @@
+export const BACK4APP_CONFIG = {
+  USE_LOCAL_DATA: true,
+  APP_ID: "8HhEXEVMZFFaGHhcaihA27sDNtYwDqFu6YdCWLgz",
+  API_KEY: "Y0fgejlcQWw2JeiD2h5QsYFxbLC1Tjmdr42igO0g",
+  BASE_URL: "https://parseapi.back4app.com/classes/Pokemon?limit=151",
+};
